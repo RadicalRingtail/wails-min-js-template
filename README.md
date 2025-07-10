731a118f1cd296b5044dd5807f01c09532423fc4
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2025-07-10 01:03:14 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minimal Vanilla JS Template
+
+## About
+
+A minimal vanilla JS template for Wails with no JS dependencies, assets, or example code. Should display a blank window with a white background upon running `wails dev`. 
+
+**Note:** `main.css` contains one line for switching the default font of the page to the system's UI font
---
+++ app.tmpl.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called at application startup
+func (a *App) startup(ctx context.Context) {
+	// Perform your setup here
+	a.ctx = ctx
+}
+
+// shutdown is called at application termination
+func (a *App) shutdown(ctx context.Context) {
+	// Perform your teardown here
+}
